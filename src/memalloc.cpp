416520fda416355233bae4bff05d1cee308c67c8
Sergio Rezende <sergiorezende2001@gmail.com>
2021-11-18 20:13:00 -0300

    Creating malloc

--- src/memalloc.cpp
+++ src/memalloc.cpp
@@ -1,12 +1,39 @@
 #include "memalloc.hpp"
 #include "core/memalloc_core.hpp"
 // Custom Compile: object file of core
-
+using namespace mm_core;
 
 namespace memalloc {
 
     void * malloc(size_t size) {
-        // Oh no, we're out of memory!
-        return (void *) -1;
+        block_t b, last;
+        // TODO: Aplicar função align para deixar os valores arredondados para potência de 2.
+        size_t s = size;
+
+        if(base) {
+            last = (block_t) base;
+            b = mm_core::find(&last, size);
+
+            if(b) {
+                if((b->size - s) >= (BLOCK_SIZE + 1 /*TODO: O número 1 deve ser trocado pelo valor do align*/))
+                    mm_core::split(b, s);
+                b->free = false;
+            } else {
+                // Verificar se o last está livre antes de extender?
+                b = extend_heap(last, s);
+                if(!b)
+                    return NULL;
+            }
+
+        }
+        else {
+            b = extend_heap(NULL, s);
+            if(!b)
+                return NULL;
+            base = b;
+        }
+
+        // Return allocated space
+        return b->data;
     }
 }
