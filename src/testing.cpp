970a0dffd4b24a7d306495c8b20cf61b027a27cb
Sergio Rezende <sergiorezende2001@gmail.com>
2021-11-18 20:13:17 -0300

    More mistakes

--- src/testing.cpp
+++ src/testing.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include "core/memalloc_core.hpp"
+#include "memalloc.hpp"
 using namespace std;
 
 int main(int argc, char **argv) {
 
-    int * arr = (int *) mm_core::malloc(sizeof(int) * 10000);
+    int * arr = (int *) memalloc::malloc(sizeof(int) * 10000);
 
     for(int i = 0; i < 10000; i++) {
         arr[i] = i;
