6e4ac83209a4f374dd9c6ee883cf3c59c61083c2
Sergio Felipe Rezende do Nascimento <sergiorezende2001@gmail.com>
2021-12-03 13:42:16 -0300

    Adicionando testes repetitivos no bash

--- run_tests.sh
+++ run_tests.sh
@@ -1,21 +1,37 @@
 rm -f "res/results.csv"
 
 echo -e "Teste base:\n"
-./testing.out --test_base
-echo
-./testing.out --test_base --std
- 
-echo -e "Teste de alocação e desalocação:\n"
-./testing.out --test_malloc_free
-echo
-./testing.out --test_malloc_free --std
+for counter in {1..5001..10}; do
+    if [ $(((counter - 1)%100)) = $((0)) ]; then 
+        echo -e '\e[1A\e[K'$((counter-1))'/5000'
+    fi
+    ./testing.out --test_base -n $((counter))
+    ./testing.out --test_base --std -n $((counter))
+done
 
-echo -e "Teste de alocação: \n"
-./testing.out --test_alloc
-echo
-./testing.out --test_alloc --std
+echo -e "\nTeste de alocação e desalocação:\n"
+for counter in {1..5001..10}; do
+    if [ $(((counter - 1)%100)) = $((0)) ]; then 
+        echo -e '\e[1A\e[K'$((counter-1))'/5000'
+    fi
+    ./testing.out --test_malloc_free -n $((counter))
+    ./testing.out --test_malloc_free --std -n $((counter))
+done
 
-echo -e "Teste de sobreposição:\n"
-./testing.out --test_overlap
-echo
-./testing.out --test_overlap --std
+echo -e "\nTeste de alocação:\n"
+for counter in {1..5001..10}; do
+    if [ $(((counter - 1)%100)) = $((0)) ]; then 
+        echo -e '\e[1A\e[K'$((counter-1))'/5000'
+    fi
+    ./testing.out --test_alloc -n $((counter))
+    ./testing.out --test_alloc --std -n $((counter))
+done
+
+echo -e "\nTeste de sobreposição:\n"
+for counter in {1..5001..10}; do
+    if [ $(((counter - 1)%100)) = $((0)) ]; then 
+        echo -e '\e[1A\e[K'$((counter-1))'/5000'
+    fi
+    ./testing.out --test_overlap -n $((counter))
+    ./testing.out --test_overlap --std -n $((counter))
+done
