c1b98dbfd24bd7456b5a7365c1b4d1ef766010d2
Sergio Felipe Rezende do Nascimento <sergiorezende2001@gmail.com>
2021-12-03 13:42:43 -0300

    Adicionando testes de sobreposição intensivo

--- run_tests.sh
+++ run_tests.sh
@@ -35,3 +35,12 @@ for counter in {1..5001..10}; do
     ./testing.out --test_overlap -n $((counter))
     ./testing.out --test_overlap --std -n $((counter))
 done
+
+echo -e "\nTeste de sobreposição intensivo:\n"
+for counter in {1..5001..10}; do
+    if [ $(((counter - 1)%100)) = $((0)) ]; then 
+        echo -e '\e[1A\e[K'$((counter-1))'/5000'
+    fi
+    ./testing.out --test_intensive_overlap -n $((counter))
+    ./testing.out --test_intensive_overlap --std -n $((counter))
+done
