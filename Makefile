f76ca74dbf67c386c07e93ad8a25750cfeded78f
Sergio Rezende <sergiorezende2001@gmail.com>
2021-12-06 20:46:11 -0300

    Adicionando mallinfo no teste

--- Makefile
+++ Makefile
@@ -11,6 +11,10 @@ debug: all
 minimum: CXXFLAGS += -DTESTING_MINIMUM -g
 minimum: objects lib clean_objects compile
 	rm ./res/minimum_results.csv
+	
+mallinfo: CXXFLAGS += -DTESTING_MALLINFO -DTESTING_MINIMUM -g 
+mallinfo: objects lib clean_objects compile
+	rm ./res/minimum_mallinfo_results.csv
 
 run:
 	./testing.out
@@ -18,18 +22,21 @@ run:
 objects:
 	$(CXX) -c src/memalloc.cpp -o memalloc.o $(CXXFLAGS)
 	$(CXX) -c src/core/memalloc_core.cpp -o memalloc_core.o $(CXXFLAGS)
-	$(CXX) -c src/core/memalloc_info.cpp -o memalloc_info.o $(CXXFLAGS)
+	$(CXX) -c src/info/memalloc_info.cpp -o memalloc_info.o $(CXXFLAGS)
+
 
 lib:
 	-mkdir bin
-	ar rvs bin/libmemalloc.a memalloc_core.o memalloc_info.o memalloc.o
+	ar rvs bin/libmemalloc.a memalloc_core.o memalloc.o memalloc_info.o
+	ar rvs bin/libmemalloc_info.a memalloc_core.o memalloc_info.o
 
 compile: 
-	$(CXX) src/testing.cpp -o bin/testing.out $(CXXFLAGS) -std=c++17 -Lbin/ -lmemalloc
+	$(CXX) src/testing.cpp -o bin/testing.out $(CXXFLAGS) -std=c++17 -Lbin/ -lmemalloc  -Lbin/ -lmemalloc_info
 
 clean: clean_objects
 	rm ./bin/testing.out
 	rm ./bin/libmemalloc.a
+	rm ./bin/libmemalloc_info.a
 
 clean_objects:
 	-rm memalloc.o
--- src/testing.cpp
+++ src/testing.cpp
@@ -10,8 +10,10 @@
 #include <string.h>
 
 #include <assert.h>
+#include <malloc.h>
 
 #include "memalloc.hpp"
+#include "info/memalloc_info.hpp"
 #include "lib/App.h"
 
 #define GREEN "\033[0;32m"
@@ -23,8 +25,28 @@
 using namespace std;
 // Memalloc Namespace
 namespace mm = memalloc;
+std::ofstream myfile;
 
-typedef int (*Test_PTR)(void * (*allocator)(size_t), void (*deallocator)(void *), size_t size);
+struct mallinfos {
+    struct mm_info::mallinfo info1;
+    struct mallinfo info2;
+    bool mymalloc;
+};
+
+void save_mallinfo(struct mallinfos *infos, string test_name, int count) {
+    if(infos->mymalloc){
+        infos->info1 = mm_info::mallinfo();
+        myfile << test_name << "," << "memmaloc" << "," << count << "," << mm::get_find() << "," << infos->info1.ordblks << "," << infos->info1.uordblks << "," 
+        << infos->info1.fordblks << "," << infos->info1.keepcost << endl;
+    }
+    else{
+        infos->info2 = mallinfo();
+        myfile << test_name << "," << "std" << "," << count << "," << "std" << "," << infos->info2.ordblks << "," << infos->info2.uordblks << "," 
+        << infos->info2.fordblks << "," << infos->info2.keepcost << endl;
+    }
+}
+
+typedef int (*Test_PTR)(void * (*allocator)(size_t), void (*deallocator)(void *), size_t size, struct mallinfos *infos);
 
 /**
  * @brief Test function for the memalloc library, it's a trivial test.
@@ -34,12 +56,20 @@ typedef int (*Test_PTR)(void * (*allocator)(size_t), void (*deallocator)(void *)
  * @param size size of the memory to allocate
  * @return int 
  */
-int test_base(void * (*allocator)(size_t), void (*deallocator)(void *), size_t size) {
+int test_base(void * (*allocator)(size_t), void (*deallocator)(void *), size_t size, struct mallinfos *infos) {
     int * ptr = (int *) allocator(sizeof(int) * size);
+
+    #ifdef TESTING_MALLINFO
+        save_mallinfo(infos, "test_base", size);
+    #endif
     
     assert(ptr != NULL);
     
     deallocator(ptr);
+
+    #ifdef TESTING_MALLINFO
+        save_mallinfo(infos, "test_base", size);
+    #endif
     return 0;
 }
 
@@ -51,10 +81,13 @@ int test_base(void * (*allocator)(size_t), void (*deallocator)(void *), size_t s
  * @param size 
  * @return int 
  */
-int test_alloc_and_dealloc(void * (*allocator)(size_t), void (*deallocator)(void *), size_t size) {
+int test_alloc_and_dealloc(void * (*allocator)(size_t), void (*deallocator)(void *), size_t size, struct mallinfos* infos) {
 
     int *integers = (int *) allocator(sizeof(int) * size);
-    
+
+    #ifdef TESTING_MALLINFO
+        save_mallinfo(infos, "test_alloc_and_dealloc", size);
+    #endif
     for (size_t i = 0; i < size; i++) {
         integers[i] = 10;
     }
@@ -65,6 +98,9 @@ int test_alloc_and_dealloc(void * (*allocator)(size_t), void (*deallocator)(void
 
     deallocator(integers);
 
+    #ifdef TESTING_MALLINFO
+        save_mallinfo(infos, "test_alloc_and_dealloc", size);
+    #endif
     return 0;
 }
 
@@ -76,10 +112,14 @@ int test_alloc_and_dealloc(void * (*allocator)(size_t), void (*deallocator)(void
  * @param size 
  * @return int 
  */
-int test_alloc(void * (*allocator)(size_t), void (*deallocator)(void *), size_t size) {
+int test_alloc(void * (*allocator)(size_t), void (*deallocator)(void *), size_t size, struct mallinfos* infos) {
 
     int *integers = (int *) allocator(sizeof(int) * size);
     
+    #ifdef TESTING_MALLINFO
+        save_mallinfo(infos, "test_alloc", size);
+    #endif
+
     for (size_t i = 0; i < size; i++) {
         integers[i] = 10;
     }
@@ -99,16 +139,20 @@ int test_alloc(void * (*allocator)(size_t), void (*deallocator)(void *), size_t 
  * @param argv 
  * @return int 
  */
-int test_overlap(void * (*allocator)(size_t), void (*deallocator)(void *), size_t size) {
+int test_overlap(void * (*allocator)(size_t), void (*deallocator)(void *), size_t size, struct mallinfos* infos) {
     
         int *integers1 = (int *) allocator(sizeof(int) * size);
         
         for (size_t i = 0; i < size; i++) {
             integers1[i] = 10;
         }
-
+        #ifdef TESTING_MALLINFO
+            save_mallinfo(infos, "test_overlap", size);
+        #endif
         int *integers2 = (int *) allocator(sizeof(int) * size);
-
+        #ifdef TESTING_MALLINFO
+            save_mallinfo(infos, "test_overlap", size);
+        #endif
         for (size_t i = 0; i < size; i++) {
             integers2[i] = 5;
         }
@@ -121,22 +165,27 @@ int test_overlap(void * (*allocator)(size_t), void (*deallocator)(void *), size_
 
         deallocator(integers1);
         deallocator(integers2);
-
+        #ifdef TESTING_MALLINFO
+            save_mallinfo(infos, "test_overlap", size);
+        #endif
         return 0;
 }
 
 
-int test_intensive_overlap(void * (*allocator)(size_t), void (*deallocator)(void *), size_t size) {
-    
+int test_intensive_overlap(void * (*allocator)(size_t), void (*deallocator)(void *), size_t size, struct mallinfos* infos) {
         for(size_t n = 1; n < size; n++) {
             int *integers1 = (int *) allocator(sizeof(int) * n);
-
+            #ifdef TESTING_MALLINFO
+                save_mallinfo(infos, "test_intensive_overlap", size);
+            #endif
             for (size_t i = 0; i < n; i++) {
                 integers1[i] = 10;
             }
             
             int *integers2 = (int *) allocator(sizeof(int) * n);
-
+            #ifdef TESTING_MALLINFO
+                save_mallinfo(infos, "test_intensive_overlap", size);
+            #endif
             for (size_t i = 0; i < n; i++) {
                 integers2[i] = 5;
             }
@@ -148,6 +197,9 @@ int test_intensive_overlap(void * (*allocator)(size_t), void (*deallocator)(void
 
             deallocator(integers1);
             deallocator(integers2);
+            #ifdef TESTING_MALLINFO
+                save_mallinfo(infos, "test_intensive_overlap", size);
+            #endif
         }
 
         return 0;
@@ -158,9 +210,11 @@ int main(int argc, char **argv) {
     string find_type;
     Test_PTR test = test_alloc;
     string test_name;
+    struct mallinfos infos;
+    infos.mymalloc = true;
     bool print = false;
     int count = 100;
-    
+
     Stopwatch stopwatch;
     FREQUENCY(stopwatch);
 
@@ -195,9 +249,10 @@ int main(int argc, char **argv) {
     // Type of the find
     find_type = type.compare("std") == 0 ? "std" : mm::get_find();
     
-    std::ofstream myfile;
     string filename = "./res/results.csv";
-    #ifdef TESTING_MINIMUM 
+    #if defined(TESTING_MINIMUM) && defined(TESTING_MALLINFO)
+        filename = "./res/minimum_mallinfo_results.csv";
+    #elif defined(TESTING_MINIMUM)
         filename = "./res/minimum_results.csv";
     #endif
     if(std::filesystem::exists(filename)) {
@@ -205,31 +260,36 @@ int main(int argc, char **argv) {
     }
     else {
         myfile.open(filename, std::ofstream::app);
-        myfile << "test" << "," << "type" << "," << "interactions" << "," << "elapsed_time" << "," << "find_type" << endl;
+        #ifdef TESTING_MALLINFO
+            myfile << "test" << "," << "type" << "," << "interactions" << "," << "find_type" << "," 
+            << "ordblks" << "," << "uordblks" << "," << "fordblks" << "," << "keepcost" << endl;
+        #else
+            myfile << "test" << "," << "type" << "," << "interactions" << "," << "elapsed_time" << "," << "find_type" << endl;
+        #endif
     }
     /**
      * @brief Base stats 
      */
     if (print)
         cout << "Running " << YELLOW << type << COLOR_RESET << " tests with " << YELLOW << count << COLOR_RESET << " iterations" << endl;
-    myfile << test_name << "," << type << "," << count << ",";
 
     /**
      * @brief Just select the allocator to use and run the test
      */
     void * (*allocator)(size_t) = type.compare("std") == 0 ? malloc : mm::malloc;
     void (*deallocator)(void *) = type.compare("std") == 0 ? free : mm::free;
+    infos.mymalloc = type.compare("std") == 0 ? false : true;
 
     if(print)
         cout << "Batch of tests in " << type << endl;
     
-    cout << RED;
+    cout << RED;                
     
     START_STOPWATCH(stopwatch);
     #ifdef TESTING_MINIMUM 
-        test(allocator, deallocator, count);
+        test(allocator, deallocator, count, &infos);
     #else
-        for(int i = 1; i <= count; i += 1)  test(allocator, deallocator, i * 5);
+        for(int i = 1; i <= count; i += 1)  test(allocator, deallocator, i * 5, &infos);
     #endif
     STOP_STOPWATCH(stopwatch);
 
@@ -238,5 +298,7 @@ int main(int argc, char **argv) {
 
     if(print)
         cout << GREEN << "Elapsed Time: " << std::to_string(stopwatch.mElapsedTime) << COLOR_RESET << endl;
-    myfile << std::fixed << std::setprecision(10) << stopwatch.mElapsedTime << "," << find_type << std::endl;
+    #ifndef TESTING_MALLINFO
+        myfile << test_name << "," << type << "," << count << "," << std::fixed << std::setprecision(10) << stopwatch.mElapsedTime << "," << find_type << std::endl;
+    #endif
 }
